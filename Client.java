3256c73a637faf5215eb44479e4581e510173652
Samuele Auteri <104078093+samutigro@users.noreply.github.com>
2023-06-06 17:41:36 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,3 +1,5 @@
+//abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;':",.<>/?`
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
